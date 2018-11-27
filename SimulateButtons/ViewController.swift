763680901e99e7d1ac07d58e17fd23e24691748e
meru_kuroneko <canopy.4160+github@gmail.com>
2018-11-27 22:47:32 +0900

    Initial Commit

---
+++ SimulateButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimulateButtons
+//
+//  Created by 田村みゆき on 2018/11/27.
+//  Copyright © 2018 Miyuki Hirose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
